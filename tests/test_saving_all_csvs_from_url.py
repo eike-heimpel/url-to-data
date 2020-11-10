90a7db6f21818f16baac67d9a566f0297af0e464
Eike LALA <eike@heimpel.family>
2020-11-10 09:47:29 +0100

    adding test infrastructure

---
+++ tests/test_saving_all_csvs_from_url.py
@@ -0,0 +1,8 @@
+from url_to_data import all_csvs_to_file
+
+print("hello")
+exit()
+
+url = "https://statweb.stanford.edu/~sabatti/data.html"
+
+all_csvs_to_file(url, "C:/Users/eikeh/Desktop/Python_Projects/KriminalStats/data/test17")
