8813daef3e4ddb26a3231e228cb109904cb16fa4
Eike LALA <eike@heimpel.family>
2020-11-10 12:14:16 +0100

    adding full csv download test

--- tests/test_saving_all_csvs_from_url.py
+++ tests/test_saving_all_csvs_from_url.py
@@ -1,8 +1,19 @@
+import pytest
+import sys, os
+sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 from url_to_data import all_csvs_to_file
 
-print("hello")
-exit()
+@pytest.fixture
+def list_of_test_urls_with_csvs():
 
-url = "https://statweb.stanford.edu/~sabatti/data.html"
+    url_list =  ["https://statweb.stanford.edu/~sabatti/data.html",
+            "https://www.bka.de/DE/AktuelleInformationen/StatistikenLagebilder/PolizeilicheKriminalstatistik/PKS2019/PKSTabellen/BundFalltabellen/bundfalltabellen.html?nn=130872",
+            "https://ourworldindata.org/coronavirus-source-data"]
 
-all_csvs_to_file(url, "C:/Users/eikeh/Desktop/Python_Projects/KriminalStats/data/test17")
+    return url_list
+
+
+def test_saving_all_csvs(list_of_test_urls_with_csvs):
+
+    for url in list_of_test_urls_with_csvs:
+        all_csvs_to_file(url, "C:/Users/eikeh/Desktop/Python_Projects/KriminalStats/data/test17")
